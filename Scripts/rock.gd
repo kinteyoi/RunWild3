8976ea3204bc391d0acff87facf831f0f3829677
ChaneyWilliams <chaneyw2004@gmail.com>
2025-09-26 21:45:26 -0500

    Most of the swimming game

--- Scripts/rock.gd
+++ Scripts/rock.gd
@@ -1,16 +1,19 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var speed = 100
-var direction: Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.DOWN * 200
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	velocity = direction.normalized() * speed
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	move_and_collide(velocity * delta)
+	position += direction * delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_body_entered(body) -> void:
+	print("ehaw")
+	body.queue_free()
