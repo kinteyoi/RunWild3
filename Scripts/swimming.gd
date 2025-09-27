f4bc6f10774b6eb0090f445121c11f3442aa68ea
ChaneyWilliams <chaneyw2004@gmail.com>
2025-09-26 22:17:07 -0500

    swim done

--- Scripts/swimming.gd
+++ Scripts/swimming.gd
@@ -3,8 +3,12 @@ extends Node2D
 @onready var rocks: Node2D = $Rocks
 @onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
 @onready var mini_game_player: CharacterBody2D = $MiniGamePlayer
+@onready var rock_timer: Timer = $RockTimer
+
+@export var power = 0
 
 func _process(delta: float) -> void:
+	power += delta
 	if mini_game_player == null:
 		Engine.time_scale = 0
 
@@ -14,3 +18,8 @@ func _on_rock_timer_timeout() -> void:
 	path_follow_2d.progress_ratio = randPos
 	rock.global_position = path_follow_2d.global_position
 	rocks.add_child(rock)
+
+
+func _on_difficulty_timeout() -> void:
+	rock_timer.wait_time -= 0.1
+	print(rock_timer.wait_time)
