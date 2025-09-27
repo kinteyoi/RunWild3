4091d40c1fdef11479473d6d59a42973d9ca3ca6
kinteyoi <calebazlin5@gmail.com>
2025-09-26 21:13:34 -0500

    fonts, eggs, hatching

--- Scripts/hatch.gd
+++ Scripts/hatch.gd
@@ -2,7 +2,18 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var jump: AnimationPlayer = $Jump
 
+
+@onready var egg1 = preload("res://Assets/Eggs/runwild_egg1.png")
+@onready var egg2 = preload("res://Assets/Eggs/runwild_egg2green.png")
+@onready var egg3 = preload("res://Assets/Eggs/runwild_egg3purple.png")
+@onready var egg4 = preload("res://Assets/Eggs/runwild_egg4blue.png")
+@onready var egg5 = preload("res://Assets/Eggs/runwild_egg5orange.png")
+@onready var egg: Sprite2D = $Egg
+
 func _ready() -> void:
+	var eggs = [egg1, egg2, egg3, egg4, egg5]
+	var random_item = eggs.pick_random()
+	egg.texture = random_item
 	wiggle()
 
 func _on_wiggle_timeout() -> void:
