fe03bed4f8601f56852e8cad9c351d2933e84d83
kinteyoi <calebazlin5@gmail.com>
2025-09-28 01:45:42 -0500

    fruit

--- Scripts/fruit.gd
+++ Scripts/fruit.gd
@@ -2,15 +2,18 @@ extends CharacterBody2D
 
 @onready var apple1 = preload("res://Assets/Environment/runwild_apple1.png")
 @onready var apple2 = preload("res://Assets/Environment/runwild_apple2.png")
+@onready var apple3 = preload("res://Assets/Environment/runwild_appleEVIL.png")
 @export var speed = 1
 var time_factor = 1
 @onready var sprite_2d: Sprite2D = $Sprite2D
-
+var is_evil = false
 var direction: Vector2 = Vector2.DOWN
 
 func _ready() -> void:
-	var app_graphics = [apple1, apple2]
+	var app_graphics = [apple1, apple2, apple3]
 	var random_item = app_graphics.pick_random()
+	if random_item == apple3:
+		is_evil = true
 	sprite_2d.texture = random_item
 
 func _physics_process(delta: float) -> void:
@@ -25,6 +28,8 @@ func _process(delta: float) -> void:
 	velocity = direction.normalized() * speed
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if is_evil:
+		print("ouch")
 	Manager.fruitStats += 100
 	queue_free()
 
--- Scripts/fruit_collection.gd
+++ Scripts/fruit_collection.gd
@@ -8,11 +8,13 @@ extends Node2D
 @onready var transition: Node2D = $Transition
 @onready var label: Label = $UI/Panel/Label
 @onready var label_2: Label = $UI/Panel/Label2
+@onready var fruit_timer: Timer = $FruitTimer
 
 signal finishedrun
 var timer = 0
 var timerLimit = 30
 var coconut
+var apple_factor = 1
 
 func _ready():
 	transition.leavesopen()
@@ -31,6 +33,7 @@ func _process(delta: float) -> void:
 		label.text = "Score: " + str(Manager.fruitStats)
 		label_2.text = "Time Left: " + str(snapped(timerLimit - timer, .01))
 func _on_fruit_timer_timeout() -> void:
+<<<<<<< Updated upstream
 	var fruitOrCoco = randi_range(0,4) == 1
 	var randPos = randf_range(0,1)
 	var fruit
@@ -45,6 +48,27 @@ func _on_fruit_timer_timeout() -> void:
 		fruit = fruitScene.instantiate()
 	fruit.global_position = path_follow_2d.global_position 
 	fruits.add_child(fruit)
+=======
+	apple_factor += .1
+	for i in apple_factor:
+		await get_tree().create_timer(.2).timeout
+		#fruit_timer.wait_time -= .03
+		print(fruit_timer.wait_time)
+		var fruitOrCoco = randi_range(0,4) == 1
+		var randPos = randf_range(0,1)
+		var fruit
+		path_follow_2d.progress_ratio = randPos
+		if fruitOrCoco:
+			if randi_range(0,2) == 1:
+				print("MOney")
+				fruit = coinScene.instantiate()
+			else:
+				fruit = coconutScene.instantiate()
+		else:
+			fruit = fruitScene.instantiate()
+		fruit.global_position = path_follow_2d.global_position 
+		get_tree().get_root().add_child(fruit)
+>>>>>>> Stashed changes
 	
 
 
