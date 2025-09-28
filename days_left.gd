88ec233d62915065e23c8d2dcfd008aa5ff27353
ChaneyWilliams <chaneyw2004@gmail.com>
2025-09-28 16:35:01 -0500

    yep

--- days_left.gd
+++ days_left.gd
@@ -1,6 +1,8 @@
 extends Node2D
 @onready var label: Label = $Marker2D/Label
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var label_2: Label = $Marker2D/Label2
+
 signal finished
 func _ready() -> void:
 	label.text = str(Manager.days_left) + " Days Left"
@@ -8,9 +10,11 @@ func _ready() -> void:
 		node.queue_free()
 	if Manager.days_left == 8:
 		Manager.evo = 2 
+		label_2.visible = true
 	elif Manager.days_left == 4:
 		Manager.evo = 3
-	print("days" + str(Manager.days_left) + " evo" + str(Manager.evo))
+		label_2.visible = true
+	
 
 
 
