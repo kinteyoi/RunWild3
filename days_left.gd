64894731e5e962a1079d1698112a382056ba157a
ChaneyWilliams <chaneyw2004@gmail.com>
2025-09-28 13:09:43 -0500

    fixed dayleft

--- days_left.gd
+++ days_left.gd
@@ -6,6 +6,12 @@ func _ready() -> void:
 	label.text = str(Manager.days_left) + " Days Left"
 	for node in get_tree().get_nodes_in_group("objects"):
 		node.queue_free()
+	if Manager.days_left == 8:
+		Manager.evo = 2 
+	elif Manager.days_left == 4:
+		Manager.evo = 3
+		print("days" + str(Manager.days_left) + " evo" + str(Manager.evo))
+
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
